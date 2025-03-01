3ef56046b58ebc72c4e84e6ddc4ffd3603e1eaa4
Zero-OTP <adcano1980@gmail.com>
2025-03-01 13:40:44 +0100

    always attach to screen session

--- installsshd.sh
+++ installsshd.sh
@@ -9,13 +9,19 @@ pkg install openssh screen -y
 
 #-- Asegurarse de que el directorio existe
 mkdir -p ~/.termux/boot/
+
 #-- Crear el script de auto-arranque
 cat <<EOF > ~/.termux/boot/start-ssh
 #!/data/data/com.termux/files/usr/bin/sh
 termux-wake-lock
 sshd
-# Iniciar o adjuntar a la sesión de screen
-screen -S ssh-session -d -m || screen -r ssh-session
+# Verificar si la sesión de screen existe, si no, crearla
+if ! screen -list | grep -q "ssh-session"; then
+    # Si no existe la sesión, crearla
+    screen -S ssh-session -d -m
+fi
+# Adjuntarse a la sesión de screen
+screen -r ssh-session
 EOF
 
 # Dar permisos de ejecución al script de inicio
@@ -23,5 +29,12 @@ chmod +x ~/.termux/boot/start-ssh
 
 # Iniciar SSH
 sshd
+
+# Verificar si la sesión de screen existe, si no, crearla
+if ! screen -list | grep -q "ssh-session"; then
+    # Si no existe la sesión, crearla
+    screen -S ssh-session -d -m
+fi
+
 # Iniciar o adjuntar a la sesión de screen
-screen -S ssh-session -d -m || screen -r ssh-session
+screen -r ssh-session
